d1e02520d66478df4afca675dac3f6ba6a01b94c
Alline Oliveira allineo <alline.oliveira@gmail.com>
2020-01-05 19:27:32 -0300

    start

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from telegram.ext import Updater, CommandHandler
 
 
+def welcome(update, context):
+    message = 'Olá, ' + update.message.from_user.first_name + '!'
+    print(message)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+
 def main():
+    # token = os.getenv('TELEGRAM_BOT_TOKEN', None)
     token = 'cole_aqui_o_token_de_acesso_do_seu_bot'
     updater = Updater(token=token, use_context=True)
 
+    updater.dispatcher.add_handler(CommandHandler('start', welcome))
+
     updater.start_polling()
+    print(str(updater))
     updater.idle()
 
 

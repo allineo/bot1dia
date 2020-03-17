67d79cdee740bbc80341f5c557f98975cf913537
Alline Oliveira allineo <alline.oliveira@gmail.com>
2020-03-17 16:14:28 -0300

    feedback

--- bot.py
+++ bot.py
@@ -1,12 +1,11 @@
 import requests
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
-                          RegexHandler, ConversationHandler)
+                         RegexHandler, ConversationHandler)
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 
 
 STATE1 = 1
 STATE2 = 2
-
 
 def welcome(update, context):
     try:
@@ -14,8 +13,7 @@ def welcome(update, context):
         firstName = update.message.from_user.first_name
         lastName = update.message.from_user.last_name
         message = 'Olá, ' + firstName + '!'
-        context.bot.send_message(
-            chat_id=update.effective_chat.id, text=message)
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
     except Exception as e:
         print(str(e))
 
@@ -23,26 +21,29 @@ def welcome(update, context):
 def feedback(update, context):
     try:
         message = 'Por favor, digite um feedback para o nosso tutorial:'
-        update.message.reply_text(
-            message, reply_markup=ReplyKeyboardMarkup([], one_time_keyboard=True))
+        update.message.reply_text(message, reply_markup=ReplyKeyboardMarkup([], one_time_keyboard=True)) 
         return STATE1
     except Exception as e:
         print(str(e))
 
 
 def inputFeedback(update, context):
-    try: 
-        feedback = update.message.text
-        print(feedback)
-        if len(feedback) > 10:
-            message = """Seu feedback foi muito curtinho... 
-                            \nInforma mais pra gente, por favor?"""
-            update.message.reply_text(message, reply_markup=ReplyKeyboardMarkup([], one_time_keyboard=True))
-            return STATE2
-        else:
-            update.message.reply_text("Muito obrigada pelo seu feedback!")
-    except Exception as e:
-        print(str(e))
+    feedback = update.message.text
+    print(feedback)
+    if len(feedback) < 10:
+        message = """Seu feedback foi muito curtinho... 
+                        \nInforma mais pra gente, por favor?"""
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+        return STATE1
+    else:
+        message = "Muito obrigada pelo seu feedback!"
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+
+def inputFeedback2(update, context):
+    feedback = update.message.text
+    message = "Muito obrigada pelo seu feedback!"
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
 
 def cancel(update, context):
@@ -61,16 +62,13 @@ def main():
             entry_points=[CommandHandler('feedback', feedback)],
             states={
                 STATE1: [MessageHandler(Filters.text, inputFeedback)],
-                STATE2: [MessageHandler(Filters.regex(
-                    '^(Digitar novamente|Permanecer este feedback)$'), inputFeedback)]
+                STATE2: [MessageHandler(Filters.text, inputFeedback2)]
             },
-            fallbacks=[CommandHandler('cancel', cancel)]
-        )
+            fallbacks=[CommandHandler('cancel', cancel)])
         updater.dispatcher.add_handler(conversation_handler)
 
-        # updater.dispatcher.add_error_handler(error)
+        print("Updater no ar1: " + str(updater))
         updater.start_polling()
-        print("Updater no ar: " + str(updater))
         updater.idle()
     except Exception as e:
         print(str(e))
